0b02a17c1c05c5f32819d192c07af721150a26f4
husihui <husihui-jk@360jinrong.net>
2019-08-21 12:19:19 +0800

    暂存

--- src/pages/cost/dropdown/dropdown.vue
+++ src/pages/cost/dropdown/dropdown.vue
@@ -1,34 +1,54 @@
 <template>
-<div class="cs-dropdown" @click="switchDropMenu">
-  <span class="demo-content">1234567890</span>
-  <div :class="`drop-menu ${dropMenuStyle}`">
-    ABC
+<div class="cs-dropdown">
+    <div
+    :class="`cs-input input__${uuid}`"
+    @click="switchInputStatus">
+    <input
+      @blur="onInputBlur"
+      :class="`${focusedStyle}`" />
+    <span class="cs-input-suffix">
+      <i class="el-icon-iconName el-icon-arrow-down" />
+    </span>
   </div>
+  <cs-drop-menu
+    :isMenuOpen="isFocus"
+  />
 </div>
 </template>
 <script>
+import CsDropMenu from './dropmenu'
+import CsInput from '../input/input'
+import uuid from '../utils/uuid'
+
 export default {
   name: 'CsDropdown',
+  components: { CsDropMenu, CsInput },
   data () {
     return {
-      isDropMenuOpen: false
+      uuid: '',
+      isFocus: false
     }
   },
   mounted: function () {
     // 获取附着组件的相对浏览器坐标
+    this.uuid = uuid()
   },
   computed: {
-    dropMenuStyle: function () {
-      return (this.isDropMenuOpen ? 'drop-menu-open' : 'drop-menu-hide')
+    focusedStyle: function () {
+      return (this.isFocus ? 'input_focus' : 'input_unfocus')
     }
   },
   methods: {
-    switchDropMenu: function () {
-      if (this.isDropMenuOpen) {
-        this.isDropMenuOpen = false
+    switchInputStatus: function () {
+      if (!this.isFocus) {
+        this.isFocus = true
       } else {
-        this.isDropMenuOpen = true
+        this.isFocus = false
+        document.querySelector(`.input__${this.uuid} input`).blur()
       }
+    },
+    onInputBlur: function () {
+      this.isFocus = false
     }
   }
 }
@@ -36,25 +56,50 @@ export default {
 <style lang="less">
 .cs-dropdown{
   cursor: pointer;
-  background: lightblue;
   display: inline-block;
   position: relative;
-  .drop-menu{
-    width: 100%;
-    cursor: default;
-    height: 500px;
-    position: absolute;
-  }
-  .drop-menu-open{
-    display: block;
-  }
-  .drop-menu-hide{
-    display: none;
-  }
   .demo-content{
     width: 180px;
     background: lightgoldenrodyellow;
     display: inline-block;
+  }
+}
+.cs-input{
+  width: 180px;
+  position: relative;
+  cursor: pointer;
+  input{
+    width: 100%;
+    height: 100%;
+    padding: 5px 15px;
+    box-sizing: border-box;
+    border-radius: 4px;
+    border: 1px solid #DCDFE6;
+    outline: unset;
+    cursor: pointer;
+  }
+  .input_focus{
+    border: 1px solid #409EFF;
+    &+span{
+      transition: .3s;
+      transform: rotate(-180deg);
+    }
+  }
+  .input_unfocus{
+    border: 1px solid #DCDFE6;
+    &+span{
+      transition: .3s;
+      transform: rotate(0deg);
+    }
+  }
+  .cs-input-suffix{
+    width: 25px;
+    text-align: center;
+    position: absolute;
+    right: 5px;
+    font-size: 14px;
+    color: #C0C4CC;
+    line-height: 28px;
   }
 }
 </style>
---
+++ src/pages/cost/dropdown/dropmenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="['drop-menu', dropMenuStyle]">
+    ABC
+  </div>
+</template>
+<script>
+export default {
+  name: 'CsDropMenu',
+  props: ['isMenuOpen'],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    dropMenuStyle: function () {
+      return (this.isMenuOpen ? 'drop-menu-open' : 'drop-menu-hide')
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+<style lang="less" scoped>
+.drop-menu{
+  width: 100%;
+  position: fixed;
+  z-index: 9999;
+  margin-top: 10px;
+  cursor: default;
+  max-height: 274px;
+  position: absolute;
+  background: #ffffff;
+  padding: 6px 0;
+  border: 1px solid #E4E7ED;
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+  // transition-duration: .3s;
+  // transform-origin: center top;
+}
+.drop-menu-open{
+  display: block;
+}
+.drop-menu-hide{
+  display: none;
+}
+</style>
